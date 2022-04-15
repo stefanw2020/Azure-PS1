4e13b96b7fceadbc04458608ef215a87ef12b92c
stefanw2020 <62887909+stefanw2020@users.noreply.github.com>
2022-04-15 16:04:02 +0200

    Update Demo.ps1

--- AZAdminCert/Demo.ps1
+++ AZAdminCert/Demo.ps1
@@ -8,3 +8,5 @@ Get-AzLocation | ft
 $serviceTags = Get-AzNetworkServiceTag -Location southcentralus
 ($serviceTags.Values | Where-Object {$_.Name -eq "Storage.SouthCentralUS"}).Properties.AddressPrefixes
 ($serviceTags.Values | Where-Object {$_.Name -eq "Storage.SouthCentralUS"}).Properties.AddressPrefixes.count
+
+
